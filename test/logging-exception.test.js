2220de834b4bea26c3c99fd1a1eb63d2b66290be
hashab1926 <fikrimubarok19@gmail.com>
2023-10-26 11:43:21 +0700

    learn: winston exception

---
+++ test/logging-exception.test.js
@@ -0,0 +1,13 @@
+import winston from "winston";
+
+const logger = winston.createLogger({
+  level: "info",
+  transports: [
+    new winston.transports.Console({}),
+    new winston.transports.File({
+      handleExceptions: true,
+      filename: "exception.log"
+    }),
+  ]
+})
+hellow();
